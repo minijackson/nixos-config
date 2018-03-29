9cccfa74285ee03d29ef20cb186e124087732862
Minijackson <minijackson@riseup.net>
2018-03-30 00:11:19 +0200

    Switch to submodule mode home-manager

--- home.nix
+++ home.nix
@@ -1,19 +1,12 @@
 { ... }:
 
-let
-  rev = "4205c91609d6309ebbcddfa675fc63937718c14b";
-  homeManagerPath = builtins.fetchTarball {
-    url = "https://github.com/rycee/home-manager/archive/${rev}.tar.gz";
-    sha256 = "0aywqya116bj40qmhh6x0sz7v6iyb93rnvq93h5ifx4r9lww714j";
-  };
-in
-  {
-    imports = [
-      (import (homeManagerPath) {}).nixos
-    ];
+{
+  imports = [
+    ./home-manager/nixos
+  ];
 
-    home-manager.users.minijackson = { ... }:
-    {
-      manual.manpages.enable = true;
-    };
-  }
+  home-manager.users.minijackson = { ... }:
+  {
+    manual.manpages.enable = true;
+  };
+}
