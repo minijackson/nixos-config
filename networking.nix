4f23f6180da25424745d49cd4520634250cefcf8
Minijackson <minijackson@riseup.net>
2018-03-29 22:03:09 +0200

    Remove query logging in dnsmasq (debug time is over!)

--- networking.nix
+++ networking.nix
@@ -5,13 +5,13 @@
   # Open ports in the firewall.
   # 8080 for Kodi remote control
   # 4713 for PulseAudio TCP streaming
+  # TODO: move this out of here!
   networking.firewall.allowedTCPPorts = [ 8080 4713 ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   networking.firewall.allowPing = true;
   # Or disable the firewall altogether.
   #networking.firewall.enable = false;
 
-  # NetworkManager
   networking.networkmanager = {
     enable = true;
     # Not via NetworkManager
@@ -29,12 +29,10 @@
 
   };
 
-  # DNSMasq
   services.dnsmasq = {
     enable = false;
     extraConfig = ''
     cache-size=1000
-    log-queries
     '';
     servers = [
       # ns2.he.de
