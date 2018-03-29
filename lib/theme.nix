9727d2a8b4d8c44b219b2cc0c5406649e96fffab
Minijackson <minijackson@riseup.net>
2018-03-29 20:44:29 +0200

    Move hexcode->escape code to Nix lib + set some Less colors

---
+++ lib/theme.nix
@@ -0,0 +1,28 @@
+{ lib, ... }:
+
+with lib;
+let
+  # Convert 2 consecutive characters from a string from hexadecimal to decimal
+  hex2dec = str: start:
+    let
+      conversionTable = {
+        "0" = 0; "1" = 1; "2" = 2; "3" = 3; "4" = 4;
+        "5" = 5; "6" = 6; "7" = 7; "8" = 8; "9" = 9;
+        "a" = 10; "b" = 11; "c" = 12;
+        "d" = 13; "e" = 14; "f" = 15;
+      };
+      firstHexDigit  = toLower (elemAt (stringToCharacters str) start);
+      secondHexDigit = toLower (elemAt (stringToCharacters str) (start + 1));
+    in conversionTable.${firstHexDigit} * 16 + conversionTable.${secondHexDigit};
+
+  makeColorCode = color:
+    let genComponent = hex2dec color;
+    in "${toString (genComponent 1)};${toString (genComponent 3)};${toString (genComponent 5)}";
+
+  fgEscapeCode = hexCode: "[38;2;${makeColorCode hexCode}m";
+  bgEscapeCode = hexCode: "[48;2;${makeColorCode hexCode}m";
+in
+{
+  inherit fgEscapeCode;
+  inherit bgEscapeCode;
+}
--- shell.nix
+++ shell.nix
@@ -1,5 +1,9 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
+with import ./lib/theme.nix { inherit lib; };
+let
+  dominantEscapeCode = fgEscapeCode config.theme.colors.dominant;
+in
 {
   environment.systemPackages = with pkgs; [ zsh grml-zsh-config zsh-completions ];
 
@@ -12,15 +16,22 @@
   programs.zsh = {
     enable = true;
 
-    interactiveShellInit = ''
+    interactiveShellInit = with lib;
+    ''
       source "${pkgs.grml-zsh-config}/etc/zsh/zshrc"
 
       function () {
-        local dominant_color="${config.theme.colors.dominant}"
-        local dim_fg_color="${config.theme.colors.dimForeground}"
+        local dominant_escape_code="${dominantEscapeCode}"
+        local dim_fg_escape_code="${fgEscapeCode config.theme.colors.dimForeground}"
 
         ${builtins.readFile ./dotfiles/zshrc}
       }
+
+      # Grml's ZSH config overrides less variables
+      export ${concatStringsSep " "
+        (mapAttrsToList
+          (variable: value: "${variable}=\"${value}\"")
+          config.programs.less.envVariables)}
     '';
     promptInit = ""; # otherwise it'll override the grml prompt
 
@@ -35,4 +46,12 @@
   };
 
   users.defaultUserShell = pkgs.zsh;
+
+  programs.less = {
+    envVariables = {
+      LESS_TERMCAP_mb = dominantEscapeCode;
+      LESS_TERMCAP_md = dominantEscapeCode;
+    };
+  };
+
 }
