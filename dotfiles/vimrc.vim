e5b11f552ce907224600fde1313b8c1fa0eb3c68
Minijackson <minijackson@riseup.net>
2018-04-03 23:54:29 +0200

    MOAR Neovim config!

--- dotfiles/vimrc.vim
+++ dotfiles/vimrc.vim
@@ -9,11 +9,32 @@ set mouse=a
 set ignorecase
 set smartcase
 
+set smartindent
+set tabstop=4
+set shiftwidth=4
+
+set inccommand=split
+
+set scrolloff=1
+set sidescrolloff=5
+
+set colorcolumn=80
+
 set termguicolors
 let g:gruvbox_contrast_dark = 'soft'
 set background=dark
 colorscheme gruvbox
-exe "hi! TabLineSel guifg=" . g:dominant_color
+
+" Doesn't do anything because the tabline gets overwritten by lightline
+"exe 'hi! TabLineSel guifg=' . g:dominant_color
+
+let g:lightline = {
+			\ 'colorscheme': 'gruvbox',
+			\ 'subseparator':         { 'left': '|', 'right': '|' },
+			\ 'tab_linesubseparator': { 'left': '|', 'right': '|' },
+			\ }
+
+set noshowmode
 
 set listchars=tab:│\ ,trail:-,nbsp:+
 set list
@@ -23,9 +44,6 @@ set fillchars=fold:─,vert:│
 highlight ExtraWhitespace term=inverse cterm=inverse gui=inverse
 " Show trailing whitespace and spaces before tabs:
 autocmd Syntax * syn match ExtraWhitespace /\s\+$\| \+\ze\t/ containedin=ALL
-
-set smartindent
-set tabstop=4
 
 set completefunc=syntaxcomplete#Complete
 
--- vim.nix
+++ vim.nix
@@ -29,6 +29,33 @@
               };
             };
 
+            neco-syntax = vimUtils.buildVimPluginFrom2Nix {
+              name = "neco-syntax";
+              src = fetchgit {
+                url = "https://github.com/Shougo/neco-syntax.git";
+                rev = "98cba4a98a4f44dcff80216d0b4aa6f41c2ce3e3";
+                sha256 = "1cjcbgx3h00g91ifgw30q5n97x4nprsr4kwirydws79fcs4vkgip";
+              };
+            };
+
+            neco-vim = vimUtils.buildVimPluginFrom2Nix {
+              name = "neco-vim";
+              src = fetchgit {
+                url = "https://github.com/Shougo/neco-vim.git";
+                rev = "f5397c5e800d65a58c56d8f1b1b92686b05f4ca9";
+                sha256 = "0yb7ja6qgrazszk4i01cwjj00j9vd43zs2r11b08iy8n10jnzr73";
+              };
+            };
+
+            vim-unimpaired = vimUtils.buildVimPluginFrom2Nix {
+              name = "vim-unimpaired";
+              src = fetchgit {
+                url = "https://github.com/tpope/vim-unimpaired.git";
+                rev = "c77939c4aff30b2ed68deb1752400ec15f17c3a2";
+                sha256 = "0qd9as008r2vycls48bfb163rp7dddw7l495xn4l1gl00sh79cxy";
+              };
+            };
+
           };
 
           pluginDictionaries = [
@@ -36,21 +63,34 @@
             { name = "undotree"; }
             { name = "gruvbox"; }
             { name = "gitgutter"; }
+            { name = "lightline-vim"; }
 
             # Motions
             { name = "surround"; }
+            { name = "targets-vim"; }
 
             # Frameworks
             { name = "deoplete-nvim"; }
             { name = "LanguageClient-neovim"; }
             { name = "neoformat"; }
+            { name = "ctrlp"; }
+            # Syntax generic completion for deoplete
+            { name = "neco-syntax"; }
 
             # Languages
             { name = "vim-polyglot"; }
             { name = "editorconfig-vim"; }
+            # Vim completion for deoplete
+            { name = "neco-vim"; }
+
+            # C / C++
+            { name = "neoinclude"; }
 
             # Other
             { name = "tmux-complete"; }
+            { name = "fugitive"; }
+            { name = "repeat"; }
+            { name = "vim-unimpaired"; }
           ];
 
         };
