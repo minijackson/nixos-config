1f23ecec050e6f91e4fd5e7af60519771bb9c482
Minijackson <minijackson@riseup.net>
2018-04-16 23:32:28 +0200

    Add standalone LaTeX package + make latexmk use Make

--- usecases/latex.nix
+++ usecases/latex.nix
@@ -29,8 +29,12 @@ let
 
       todonotes
 
+      standalone
+
       # Dependencies somehow missing
       logreq xstring fvextra ifplatform framed
+      # For standalone
+      currfile
       ;
   };
 in {
@@ -57,7 +61,7 @@ in {
 
       let g:vimtex_compiler_latexmk = {}
       let g:vimtex_compiler_latexmk.build_dir = './latexmk-build'
-      let g:vimtex_compiler_latexmk.options = ['-pdf', '-verbose', '-file-line-error', '-synctex=1', '-interaction=nonstopmode', '-shell-escape', '-xelatex', '-8bit']
+      let g:vimtex_compiler_latexmk.options = ['-pdf', '-verbose', '-file-line-error', '-synctex=1', '-interaction=nonstopmode', '-shell-escape', '-use-make', '-xelatex', '-8bit']
 
       let g:vimtex_view_method = 'zathura'
       let g:vimtex_view_general_viewer = '${pkgs.zathura}/bin/zathura'
