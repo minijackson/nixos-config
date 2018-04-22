f9380879db50176968830b0d2415b3b6e1a9ea26
Minijackson <minijackson@riseup.net>
2018-04-22 10:16:40 +0200

    Set group rights in sonarr and radarr directories

--- usecases/torrent.nix
+++ usecases/torrent.nix
@@ -3,59 +3,74 @@
 let
   transmission-rpc-port = 9091;
   transmission-peer-port = 51413;
-in
-  {
-    imports = [
-      ./torrent-secret.nix
-    ];
+in {
+  imports = [
+    ./torrent-secret.nix
+  ];
 
-    services.transmission = {
-      enable = true;
-      settings = {
-        download-dir = "/var/lib/transmission/Downloads";
-        incomplete-dir = "/var/lib/transmission/.incomplete";
-        incomplete-dir-enabled = true;
+  services.transmission = {
+    enable = true;
+    settings = {
+      download-dir = "/var/lib/transmission/Downloads";
+      incomplete-dir = "/var/lib/transmission/.incomplete";
+      incomplete-dir-enabled = true;
 
-        rpc-whitelist = "127.0.0.1,192.168.*.*";
-        rpc-port = transmission-rpc-port;
-        rpc-authentication-required = true;
-        rpc-username = "transmission";
-        # Password set in torrent-secret.nix
+      rpc-whitelist = "127.0.0.1,192.168.*.*";
+      rpc-port = transmission-rpc-port;
+      rpc-authentication-required = true;
+      rpc-username = "transmission";
+      # Password set in torrent-secret.nix
 
-        blocklist-enabled = true;
-        blocklist-url = "http://list.iblocklist.com/?list=gyisgnzbhppbvsphucsw&fileformat=p2p&archiveformat=gz";
+      blocklist-enabled = true;
+      blocklist-url = "http://list.iblocklist.com/?list=gyisgnzbhppbvsphucsw&fileformat=p2p&archiveformat=gz";
 
-        trash-original-torrent-files = true;
+      trash-original-torrent-files = true;
 
-        # 2 = Required
-        encryption = 2;
+      # 2 = Required
+      encryption = 2;
 
-        peer-port = transmission-peer-port;
-        peer-socket-tos = "lowcost";
+      peer-port = transmission-peer-port;
+      peer-socket-tos = "lowcost";
 
-        download-queue-size = 10;
+      download-queue-size = 10;
 
-        ratio-limit = 2.0;
-        ratio-limit-enabled = true;
-      };
+      ratio-limit = 2.0;
+      ratio-limit-enabled = true;
     };
+  };
 
-    services.radarr.enable = true;
-    services.sonarr.enable = true;
-    services.jackett.enable = true;
+  services.radarr.enable = true;
+  services.sonarr.enable = true;
+  services.jackett.enable = true;
 
-    users.users.radarr.extraGroups = [ "transmission" ];
-    users.users.sonarr.extraGroups = [ "transmission" ];
+  users.users.radarr.extraGroups = [ "transmission" ];
+  users.users.sonarr.extraGroups = [ "transmission" ];
 
-    users.users.minijackson.extraGroups = [ "transmission" "sonarr" "radarr" ];
+  users.users.minijackson.extraGroups = [ "transmission" "sonarr" "radarr" ];
 
-    networking.firewall.allowedTCPPorts = [
-      transmission-rpc-port transmission-peer-port
-      # Sonarr WebUI
-      8989
-      # Radarr WebUI
-      7878
-      # Jackett WebUI
-      9117
-    ];
-  }
+  networking.firewall.allowedTCPPorts = [
+    transmission-rpc-port transmission-peer-port
+    # Sonarr WebUI
+    8989
+    # Radarr WebUI
+    7878
+    # Jackett WebUI
+    9117
+  ];
+
+  systemd.services.sonarr_radarr_rights = {
+    description = "Allow access to users in the sonarr / radarr groups";
+    after = [ "sonarr.service" "radarr.service" ];
+    # Restart of sonarr or radarr are restarted
+    partOf = [ "sonarr.service" "radarr.service" ];
+    wantedBy = [ "multi-user.target" ];
+
+    script = ''
+      ${pkgs.coreutils}/bin/chmod 770 /var/lib/sonarr/ /var/lib/radarr/
+    '';
+
+    serviceConfig = {
+      Type = "oneshot";
+    };
+  };
+}
