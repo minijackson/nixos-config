f58a6bb9891985d0aedad6141f44a4544c2007ae
Minijackson <minijackson@riseup.net>
2018-04-28 13:10:23 +0200

    Add XDG user-dirs

--- home.nix
+++ home.nix
@@ -521,7 +521,20 @@ in
       };
     };
 
-    xdg.configFile."alacritty/alacritty.yml".source = ./dotfiles/alacritty.yml;
+    xdg.configFile = {
+      "alacritty/alacritty.yml".source = ./dotfiles/alacritty.yml;
+
+      "user-dirs.dirs".text = ''
+        XDG_DESKTOP_DIR="$HOME/Desktop"
+        XDG_DOCUMENTS_DIR="$HOME/Documents"
+        XDG_DOWNLOAD_DIR="$HOME/Downloads"
+        XDG_MUSIC_DIR="$HOME/Music"
+        XDG_PICTURES_DIR="$HOME/Pictures"
+        XDG_PUBLICSHARE_DIR="$HOME/Public"
+        XDG_TEMPLATES_DIR="$HOME/Templates"
+        XDG_VIDEOS_DIR="$HOME/Videos"
+      '';
+    };
 
     gtk = {
       enable = true;
