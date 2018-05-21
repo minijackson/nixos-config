50a6aaac23234625664b6cdcdcba58854be37cd0
Minijackson <minijackson@riseup.net>
2018-05-21 11:44:57 +0200

    Register zsh-history-substring-search in packages

--- packages/default.nix
+++ packages/default.nix
@@ -3,4 +3,5 @@
 {
   cquery = (import ./cquery { inherit pkgs; }).cquery;
   tomorrowPygments = (import ./tomorrow-pygments { inherit pkgs; }).tomorrowPygments;
+  zsh-history-substring-search = (import ./zsh-history-substring-search { inherit pkgs; }).zsh-history-substring-search;
 }
