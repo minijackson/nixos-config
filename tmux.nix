481b9e960a9d3261f3d5d0317793e8d012fbf60f
Minijackson <minijackson@riseup.net>
2019-06-23 21:20:29 +0200

    tmux: now with plugins and SSH conf

--- tmux.nix
+++ tmux.nix
@@ -1,6 +1,16 @@
 { config, pkgs, ... } :
 
-{
+let
+  # Thanks: https://github.com/DanielFGray/dotfiles/blob/master/tmux.remote.conf
+  remoteConf = builtins.toFile "tmux.remote.conf" ''
+    unbind C-q
+    unbind q
+    set-option -g prefix C-s
+    bind s send-prefix
+    bind C-s last-window
+    set-option -g status-position top
+  '';
+in {
 
   imports = [ ./theme.nix ];
 
@@ -12,8 +22,20 @@
     terminal = "screen-256color";
     historyLimit = 50000;
 
-    extraTmuxConf = with config.theme;
+    extraTmuxConf = with config.theme; with pkgs.tmuxPlugins;
     ''
+      # Plugins
+      run-shell '${copycat}/share/tmux-plugins/copycat/copycat.tmux'
+      run-shell '${sensible}/share/tmux-plugins/sensible/sensible.tmux'
+      run-shell '${urlview}/share/tmux-plugins/urlview/urlview.tmux'
+
+      if -F "$SSH_CONNECTION" "source-file '${remoteConf}'"
+
+      set-option -g status-right ' #{prefix_highlight} "#{=21:pane_title}" %H:%M %d-%b-%y'
+      set-option -g @prefix_highlight_fg '${colors.background}'
+      set-option -g @prefix_highlight_bg '${colors.dominant}'
+      run-shell '${prefix-highlight}/share/tmux-plugins/prefix-highlight/prefix_highlight.tmux'
+
       # Be faster switching windows
       bind C-n next-window
       bind C-p previous-window
@@ -31,7 +53,6 @@
       bind '"' split-window -v -c "#{pane_current_path}"
 
       # Colorscheme
-
       set-option -g status-style 'fg=${colors.dimForeground}, bg=${colors.background}'
 
       set-option -g window-status-current-fg '${colors.dominant}'
